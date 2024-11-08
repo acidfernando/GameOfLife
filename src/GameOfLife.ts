30170b5474d65d87c511509974f81a11d950a71c
Fernando Andrés García Hernández <f.garcia.ext@docline.com>
2024-11-08 18:29:26 +0000

    green: only kills any living cell if condition is met

--- src/GameOfLife.ts
+++ src/GameOfLife.ts
@@ -6,21 +6,47 @@ export class GameOfLife {
   }
 
   run(iterations = 1) {
-    for(let i = 0; i<iterations; i++) {
+    for (let i = 0; i < iterations; i++) {
       this.runIteration()
     }
   }
 
   private runIteration() {
-    let newBoard = this.board;
-    for(let i = 0; i<this.board.length; i++) {
-      for(let j = 0; j<this.board[i].length; j++) {
-        newBoard[i][j] = this.getNextCellStatus()
+    let newBoard = this.board
+    for (let row = 0; row < this.board.length; row++) {
+      for (let column = 0; column < this.board[row].length; column++) {
+        newBoard[row][column] = this.getNextCellStatus(row, column)
       }
     }
   }
 
-  private getNextCellStatus() {
-    return "x"
+  private getNextCellStatus(row: number, column: number) {
+    const cellStatus = this.board[row][column]
+
+    if (cellStatus === "O") {
+      const liveNeighbours = this.getLiveNeighbours(row, column)
+
+      if (liveNeighbours < 2) {
+        return "x"
+      }
+    }
+
+    return cellStatus
+  }
+
+  private getLiveNeighbours(row: number, column: number) {
+    let liveNeighbours = 0
+
+    for (let i = row - 1; i <= row + 1; i++) {
+      for (let j = column - 1; j <= column + 1; j++) {
+        if (i === row && j === column) continue
+
+        if (this.board[i]?.[j] === "O") {
+          liveNeighbours++
+        }
+      }
+    }
+
+    return liveNeighbours
   }
 }
